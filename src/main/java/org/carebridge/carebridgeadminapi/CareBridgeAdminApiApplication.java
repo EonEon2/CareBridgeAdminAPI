343f105b5511c8ffc8636340d73c4a06e017174f
jhyeong <hyeongcode@gmail.com>
2024-10-21 10:37:04 +0900

    feat: init

---
+++ src/main/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplication.java
@@ -0,0 +1,13 @@
+package org.carebridge.carebridgeadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareBridgeAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareBridgeAdminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carebridge.carebridgeadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareBridgeAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
