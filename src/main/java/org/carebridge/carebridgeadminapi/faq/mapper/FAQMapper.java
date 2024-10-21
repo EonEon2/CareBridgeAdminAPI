7c5cd5d2642a466718675c2802d1b585d37ae3cb
yun9699 <cha23215@daum.net>
2024-10-21 14:58:27 +0900

    docs FAQDTO add

--- src/main/java/org/carebridge/carebridgeadminapi/faq/mapper/FAQMapper.java
+++ src/main/java/org/carebridge/carebridgeadminapi/faq/mapper/FAQMapper.java
@@ -1,19 +1,22 @@
 package org.carebridge.carebridgeadminapi.faq.mapper;
 
+import org.carebridge.carebridgeadminapi.faq.dto.FAQDTO;
+import java.util.List;
+
 public interface FAQMapper {
 
-//    // 전체 FAQ 목록 조회
-//    List<FAQ> getAllFAQs();
-//
-//    //FAQ 목록 페이징 조회
-//    List<FAQ> getFAQsPaged(int offset, int limit);
-//
-//    // 새로운 FAQ 추가
-//    int insertFAQ(FAQ faq);
-//
-//    // 기존 FAQ 수정
-//    int updateFAQ(FAQ faq);
-//
-//    // 특정 FAQ 삭제
-//    int deleteFAQ(int id);
+    // 전체 FAQ 목록 조회
+    List<FAQDTO> getAllFAQs();
+
+    // FAQ 목록 페이징 조회
+    List<FAQDTO> getFAQsPaged(int offset, int limit);
+
+    // 새로운 FAQ 추가
+    int insertFAQ(FAQDTO faq);
+
+    // 기존 FAQ 수정
+    int updateFAQ(FAQDTO faq);
+
+    // 특정 FAQ 삭제
+    int deleteFAQ(int id);
 }
