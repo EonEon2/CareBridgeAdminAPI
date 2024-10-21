84473ea8315683e5ce50053efb3861da07d4e7d1
DSL0680 <ekthf988172@gmail.com>
2024-10-21 14:12:15 +0900

    2

--- src/test/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplicationTests.java
+++ src/test/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplicationTests.java
@@ -1,6 +1,7 @@
 package org.carebridge.carebridgeadminapi;
 
 import lombok.extern.log4j.Log4j2;
+import org.carebridge.carebridgeadminapi.qna.mapper.QNAMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
