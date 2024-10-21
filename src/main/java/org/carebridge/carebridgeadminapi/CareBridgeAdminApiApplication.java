4f84c996d0b990db1dc633cc57bc3b9986456150
Dongeon Lee <duqdjs123@naver.com>
2024-10-21 13:36:56 +0900

    feat: baseSetting

--- src/main/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplication.java
+++ src/main/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplication.java
@@ -1,9 +1,12 @@
 package org.carebridge.carebridgeadminapi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan(basePackages = {"org.carebridge.carebridgeadminapi.**.mapper"})
 public class CareBridgeAdminApiApplication {
 
     public static void main(String[] args) {
--- src/test/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplicationTests.java
+++ src/test/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplicationTests.java
@@ -1,10 +1,23 @@
 package org.carebridge.carebridgeadminapi;
 
+import lombok.extern.log4j.Log4j2;
+import org.carebridge.carebridgeadminapi.time.mapper.TimeMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Log4j2
 class CareBridgeAdminApiApplicationTests {
+
+    @Autowired(required = false)
+    TimeMapper timeMapper;
+
+    @Test
+    public void Test1(){
+        log.info("------------");
+        log.info("!"+timeMapper.getTime());
+    }
 
     @Test
     void contextLoads() {
