d5102f24a995c55cf8736646c71fef34a50a856c
yun9699 <cha23215@daum.net>
2024-10-22 10:30:06 +0900

    feat faqvo merge2

--- src/test/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplicationTests.java
+++ src/test/java/org/carebridge/carebridgeadminapi/CareBridgeAdminApiApplicationTests.java
@@ -3,7 +3,6 @@ package org.carebridge.carebridgeadminapi;
 import lombok.extern.log4j.Log4j2;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
